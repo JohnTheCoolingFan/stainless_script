941236abf97ab6cf6b96d029736b214f8e5a8213
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-11-07 14:48:09 +0300

    Print node
    Now you can print "Hello, world!"

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -1,5 +1,6 @@
-use crate::{Class, ExecutionContext, Node, Object, OutputSocket, InputSocket};
-use std::rc::Rc;
+use crate::{Class, ExecutionContext, InputSocket, Node, Object, OutputSocket};
+use std::{fmt::Display, num::ParseIntError, rc::Rc, str::FromStr, ops::Deref};
+use thiserror::Error;
 
 pub fn nop_node_class() -> Class {
     Class {
@@ -26,6 +27,13 @@ pub fn any_class() -> Class {
     Class {
         name: "any".into(),
         default_node: Rc::new(NopNode) as Rc<dyn Node>,
+    }
+}
+
+pub fn print_class() -> Class {
+    Class {
+        name: "print".into(),
+        default_node: Rc::new(Print(PrintVariant { ln: true, amount: 1 })) as Rc<dyn Node>
     }
 }
 
@@ -119,9 +127,7 @@ impl Node for BoolConstructor {
     fn set_variant(&mut self, _variant: &str) {}
 
     fn inputs(&self) -> Vec<InputSocket> {
-        vec![InputSocket {
-            class: any_class()
-        }]
+        vec![InputSocket { class: any_class() }]
     }
 
     fn outputs(&self) -> Vec<OutputSocket> {
@@ -163,7 +169,7 @@ impl Node for IfNode {
 
     fn inputs(&self) -> Vec<InputSocket> {
         vec![InputSocket {
-            class: bool_class()
+            class: bool_class(),
         }]
     }
 
@@ -173,5 +179,112 @@ impl Node for IfNode {
 
     fn branches(&self) -> usize {
         2
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct PrintVariant {
+    ln: bool,
+    amount: usize,
+}
+
+impl Display for PrintVariant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{}",
+            match self.ln {
+                true => "println",
+                false => "print",
+            },
+            self.amount
+        )
+    }
+}
+
+impl FromStr for PrintVariant {
+    type Err = PrintVariantParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s == "print" {
+            Ok(Self {
+                ln: false,
+                amount: 1,
+            })
+        } else if s == "println" {
+            Ok(Self {
+                ln: true,
+                amount: 1,
+            })
+        } else if let [print_kind, print_amount] = s.split(':').collect::<Vec<&str>>()[..] {
+            let ln = match print_kind {
+                "print" => false,
+                "println" => true,
+                s => return Err(PrintVariantParseError::InvalidPrintKind(s.into())),
+            };
+            let amount = print_amount.parse()?;
+            Ok(Self { ln, amount })
+        } else {
+            Err(PrintVariantParseError::InvalidVariant(s.into()))
+        }
+    }
+}
+
+#[derive(Debug, Clone, Error)]
+enum PrintVariantParseError {
+    #[error("Invalid variant: {0}")]
+    InvalidVariant(String),
+    #[error("Invalid print kind: {0}")]
+    InvalidPrintKind(String),
+    #[error("Failed to parse amount: {0}")]
+    AmountParseError(ParseIntError),
+}
+
+impl From<ParseIntError> for PrintVariantParseError {
+    fn from(e: ParseIntError) -> Self {
+        Self::AmountParseError(e)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Print(PrintVariant);
+
+impl Node for Print {
+    fn execute(&self, context: &mut ExecutionContext) -> usize {
+        let to_print: String = context.get_inputs().iter().map(ToString::to_string).collect::<Vec<String>>().join(" ");
+        if self.0.ln {
+            println!("{}", to_print);
+        } else {
+            print!("{}", to_print);
+        };
+        0
+    }
+
+    fn class(&self) -> Class {
+        print_class()
+    }
+
+    fn variants(&self) -> Vec<&str> {
+        vec!["print", "println", Box::leak(self.0.to_string().into_boxed_str())]
+    }
+
+    fn current_variant(&self) -> &str {
+        Box::leak(self.0.to_string().into_boxed_str())
+    }
+
+    fn set_variant(&mut self, variant: &str) {
+        self.0 = variant.parse().unwrap()
+    }
+
+    fn inputs(&self) -> Vec<InputSocket> {
+        vec![InputSocket { class: any_class()}; self.0.amount]
+    }
+
+    fn outputs(&self) -> Vec<OutputSocket> {
+        vec![]
+    }
+
+    fn branches(&self) -> usize {
+        1
     }
 }
