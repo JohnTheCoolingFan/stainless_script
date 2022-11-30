c67be5efde319486f650d6b64cd6fe5416707e63
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-11-30 06:00:00 +0300

    Program deserializing

--- src/bin/ssce.rs
+++ src/bin/ssce.rs
@@ -1,11 +1,18 @@
 use clap::Parser;
+use stainless_script::program::Program;
+use std::{path::PathBuf, fs::File};
+#[cfg(feature = "format-json")]
+use serde_json::from_reader as json_from_reader;
+#[cfg(feature = "format-bincode")]
+use bincode::deserialize_from as bincode_from_reader;
+use ron::de::from_reader as ron_from_reader;
 
 const LINUX_LIB_PATH: &str = "/usr/lib/stainless_script/";
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    program: String,
+    program: PathBuf,
 
     #[arg(short, long, value_enum)]
     format: Option<ProgramFormat>,
@@ -29,11 +36,33 @@ impl From<String> for ProgramFormat {
             "json" => Self::Json,
             #[cfg(feature = "format-bincode")]
             "bincode" => Self::Bincode,
-            _ => panic!("Invalid format: {}", s)
+            _ => panic!("Invalid format: {}", s),
         }
     }
 }
 
 fn main() {
     let cli = Cli::parse();
+
+    let program_format = cli.format.unwrap_or_else(|| {
+        let file_name = cli.program.file_name().unwrap().to_str().unwrap();
+        if file_name.ends_with(".ron.ssc") {
+            ProgramFormat::Ron
+        } else if cfg!(feature = "format-json") && file_name.ends_with(".json.ssc") {
+            ProgramFormat::Json
+        } else if cfg!(feature = "format-bincode") && file_name.ends_with(".bin.ssc") {
+            ProgramFormat::Bincode
+        } else {
+            panic!("Failed to determine program format based on file extension, please specify program format using --format")
+        }
+    });
+
+    let program_file = File::open(&cli.program).unwrap();
+    let program: Program = match program_format {
+        ProgramFormat::Ron => ron_from_reader(program_file).unwrap(),
+        #[cfg(feature = "format-json")]
+        ProgramFormat::Json => json_from_reader(program_file).unwrap(),
+        #[cfg(feature = "format-bincode")]
+        ProgramFormat::Bincode => bincode_from_reader(program_file).unwrap(),
+    };
 }
