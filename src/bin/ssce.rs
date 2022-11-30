501fcecfe966774677464bc99b1d27466534fcb5
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-11-30 06:32:36 +0300

    Factor out format detection and program reading

--- src/bin/ssce.rs
+++ src/bin/ssce.rs
@@ -1,11 +1,11 @@
-use clap::Parser;
-use stainless_script::program::Program;
-use std::{path::PathBuf, fs::File};
-#[cfg(feature = "format-json")]
-use serde_json::from_reader as json_from_reader;
 #[cfg(feature = "format-bincode")]
 use bincode::deserialize_from as bincode_from_reader;
+use clap::Parser;
 use ron::de::from_reader as ron_from_reader;
+#[cfg(feature = "format-json")]
+use serde_json::from_reader as json_from_reader;
+use stainless_script::program::Program;
+use std::{fs::File, path::{PathBuf, Path}};
 
 const LINUX_LIB_PATH: &str = "/usr/lib/stainless_script/";
 
@@ -41,28 +41,36 @@ impl From<String> for ProgramFormat {
     }
 }
 
-fn main() {
-    let cli = Cli::parse();
+fn format_from_filename(file_name: &str) -> ProgramFormat {
+    if file_name.ends_with(".ron.ssc") {
+        ProgramFormat::Ron
+    } else if cfg!(feature = "format-json") && file_name.ends_with(".json.ssc") {
+        ProgramFormat::Json
+    } else if cfg!(feature = "format-bincode") && file_name.ends_with(".bin.ssc") {
+        ProgramFormat::Bincode
+    } else {
+        panic!("Failed to determine program format based on file extension, please specify program format using --format")
+    }
+}
 
-    let program_format = cli.format.unwrap_or_else(|| {
-        let file_name = cli.program.file_name().unwrap().to_str().unwrap();
-        if file_name.ends_with(".ron.ssc") {
-            ProgramFormat::Ron
-        } else if cfg!(feature = "format-json") && file_name.ends_with(".json.ssc") {
-            ProgramFormat::Json
-        } else if cfg!(feature = "format-bincode") && file_name.ends_with(".bin.ssc") {
-            ProgramFormat::Bincode
-        } else {
-            panic!("Failed to determine program format based on file extension, please specify program format using --format")
-        }
-    });
-
-    let program_file = File::open(&cli.program).unwrap();
-    let program: Program = match program_format {
+fn read_program(path: &Path, format: &ProgramFormat) -> Program {
+    let program_file = File::open(path).unwrap();
+    match format {
         ProgramFormat::Ron => ron_from_reader(program_file).unwrap(),
         #[cfg(feature = "format-json")]
         ProgramFormat::Json => json_from_reader(program_file).unwrap(),
         #[cfg(feature = "format-bincode")]
         ProgramFormat::Bincode => bincode_from_reader(program_file).unwrap(),
-    };
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let program_format = cli.format.unwrap_or_else(|| {
+        let file_name = cli.program.file_name().unwrap().to_str().unwrap();
+        format_from_filename(file_name)
+    });
+
+    let program = read_program(&cli.program, &program_format);
 }
