8ba9f69935e141fbc6ec5dcdbd5c5954826dac58
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-11-30 04:59:23 +0300

    Add format arg

--- src/bin/ssce.rs
+++ src/bin/ssce.rs
@@ -5,7 +5,33 @@ const LINUX_LIB_PATH: &str = "/usr/lib/stainless_script/";
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    program: String
+    program: String,
+
+    #[arg(short, long, value_enum)]
+    format: Option<ProgramFormat>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
+enum ProgramFormat {
+    Ron,
+    #[cfg(feature = "format-json")]
+    Json,
+    #[cfg(feature = "format-bincode")]
+    Bincode,
+}
+
+impl From<String> for ProgramFormat {
+    fn from(s: String) -> Self {
+        match s.as_str() {
+            "ron" => Self::Ron,
+            #[cfg(feature = "format-json")]
+            "json" => Self::Json,
+            #[cfg(feature = "format-bincode")]
+            "bincode" => Self::Bincode,
+            _ => panic!("Invalid format: {}", s)
+        }
+    }
 }
 
 fn main() {
