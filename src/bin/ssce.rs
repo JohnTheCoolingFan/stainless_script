ba06b5b96535db708b68be93abbab0ed9b58202a
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-11-30 03:39:31 +0300

    CLI argument parsing

--- src/bin/ssce.rs
+++ src/bin/ssce.rs
@@ -1,5 +1,13 @@
+use clap::Parser;
+
 const LINUX_LIB_PATH: &str = "/usr/lib/stainless_script/";
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    program: String
+}
+
 fn main() {
-    todo!()
+    let cli = Cli::parse();
 }
